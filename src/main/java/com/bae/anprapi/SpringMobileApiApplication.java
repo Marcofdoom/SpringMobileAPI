4075b5c4aa0db5d8fdf4019d2445ee22c623f6f2
Marcofdoom <marcvpartington@gmail.com>
2019-08-15 09:34:54 +0100

    first commit

---
+++ src/main/java/com/bae/anprapi/SpringMobileApiApplication.java
@@ -0,0 +1,13 @@
+package com.bae.anprapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMobileApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMobileApiApplication.class, args);
+	}
+
+}
