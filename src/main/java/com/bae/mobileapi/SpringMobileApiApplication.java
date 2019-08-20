57c741282da6ee6de5c5c02a44b46e13daa52177
ayshamarty <ayshamarty@gmail.com>
2019-08-20 17:24:12 +0100

    encryption configured for testing in vm

--- src/main/java/com/bae/mobileapi/SpringMobileApiApplication.java
+++ src/main/java/com/bae/mobileapi/SpringMobileApiApplication.java
@@ -2,7 +2,12 @@ package com.bae.mobileapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
+@Configuration
+@EnableEncryptableProperties
 @SpringBootApplication
 public class SpringMobileApiApplication {
 
