65be6c41cfaff1689e15cf31a6c1d7530ed37990
Marcofdoom <marcvpartington@gmail.com>
2019-08-29 07:28:32 +0100

    final version before presentation

--- src/main/java/com/bae/mobileapi/controller/MobileCallRecordsController.java
+++ src/main/java/com/bae/mobileapi/controller/MobileCallRecordsController.java
@@ -15,48 +15,38 @@ import java.util.List;
 import java.util.Random;
 
 @RestController
-@RequestMapping("/mobile")
+@RequestMapping("/Mobile")
 public class MobileCallRecordsController {
 
-    private MobileCallRecordsService mobileCallRecordsService;
-    private PeopleMobileService peopleMobileService;
+	private MobileCallRecordsService mobileCallRecordsService;
+	private PeopleMobileService peopleMobileService;
 
-    private int number = 0;
+	@Autowired
+	public MobileCallRecordsController(MobileCallRecordsService mobileCallRecordsService,
+			PeopleMobileService peopleMobileService) {
+		this.mobileCallRecordsService = mobileCallRecordsService;
+		this.peopleMobileService = peopleMobileService;
+	}
 
-    @Autowired
-    public MobileCallRecordsController(MobileCallRecordsService mobileCallRecordsService, PeopleMobileService peopleMobileService) {
-        this.mobileCallRecordsService = mobileCallRecordsService;
-        this.peopleMobileService = peopleMobileService;
-    }
+	@GetMapping("/geAssociates")
+	public ResponseEntity<List<MobileCallRecordsDTO>> getAssociatesByPhoneNumber(
+			@RequestParam(value = "phone-number") String phoneNumber) {
+		return new ResponseEntity<>(mobileCallRecordsService.findAssociatesByPhoneNumber(phoneNumber), HttpStatus.OK);
+	}
 
-    @GetMapping("/get-associates")
-    public ResponseEntity<List<MobileCallRecordsDTO>> getAssociatesByPhoneNumber(
-            @RequestParam(value = "phone-number") String phoneNumber) {
-        return new ResponseEntity<>(mobileCallRecordsService.findAssociatesByPhoneNumber(phoneNumber), HttpStatus.OK);
-    }
+	@GetMapping("/geMobile")
+	public ResponseEntity<List<PeopleMobile>> getAllPhoneNumbersByIdentity(
+			@RequestParam(value = "forenames") String forenames, @RequestParam(value = "surname") String surname,
+			@RequestParam(value = "homeAddress") String homeAddress,
+			@RequestParam(value = "dateOfBirth") @DateTimeFormat(pattern = "yyyy-MM-dd") Date dateOfBirth) {
 
-    @GetMapping("/get-mobile")
-    public ResponseEntity<List<PeopleMobile>> getAllPhoneNumbersByIdentity(@RequestParam(value = "forenames") String forenames,
-                                                                           @RequestParam(value = "surname") String surname,
-                                                                           @RequestParam(value = "home-address") String homeAddress,
-                                                                           @RequestParam(value = "date-of-birth") @DateTimeFormat(pattern = "yyyy-MM-dd") Date dateOfBirth) {
+		PeopleMobile peopleMobile = new PeopleMobile();
 
-        PeopleMobile peopleMobile = new PeopleMobile();
+		peopleMobile.setForenames(forenames);
+		peopleMobile.setSurname(surname);
+		peopleMobile.setHomeAddress(homeAddress);
+		peopleMobile.setDateOfBirth(dateOfBirth);
 
-        peopleMobile.setForenames(forenames);
-        peopleMobile.setSurname(surname);
-        peopleMobile.setHomeAddress(homeAddress);
-        peopleMobile.setDateOfBirth(dateOfBirth);
-
-        return new ResponseEntity<>(peopleMobileService.findAllPhoneNumbersByIdentity(peopleMobile), HttpStatus.OK);
-    }
-
-    @PostMapping("post-data")
-    public void postData() {
-        for (int i = 0; i < 10000; i++) {
-            number++;
-            String phoneNumber = String.format("%09d", number);
-            peopleMobileService.savePeopleMobile(new PeopleMobile(phoneNumber, new Date(), "f", "h", "n", "s"));
-        }
-    }
+		return new ResponseEntity<>(peopleMobileService.findAllPhoneNumbersByIdentity(peopleMobile), HttpStatus.OK);
+	}
 }
